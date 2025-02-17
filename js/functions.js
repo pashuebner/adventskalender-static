9d54fb6d1c2877f1b543405780e8c56e60672ca7
Huebner, Pascal <info@impredia.de>
2025-02-17 17:38:36 +0100

    Search Numbers

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,3 @@
-
 let i = 1;
 
 var startScroll;
@@ -25,7 +24,49 @@ $(document).ready(function () {
     event.preventDefault();
   });
   calender();
+  checkNumber();
 });
+
+function checkNumber() {
+  if ($(".kalender-grid")[0]) {
+    var gNumbers = [];
+    const numberElem = $(".modal table tr");
+    numberElem.each(function () {
+      var gNumbersAdd = {
+        price: $(this).find("td").eq(0).text().trim(),
+        value: $(this).find("td").eq(1).text().trim(),
+        number: $(this).find("td").eq(2).text().trim(),
+      };
+      gNumbers.push(JSON.stringify(gNumbersAdd));
+    });
+    console.log(gNumbers);
+    $(".checkNumberSubmit").click(function () {
+      let inputVal = $(".checkNumber").val().trim();
+      let match = gNumbers.find((item) => {
+        let obj = JSON.parse(item);
+        return parseInt(obj.number) === parseInt(inputVal);
+      });
+      if (match) {
+        let matchedObj = JSON.parse(match);
+        $(".checkResult > div").html(
+          `<h2>Gewonnen!</h2><p><strong>${matchedObj.price}</strong> </br>im Wert von <strong> ${matchedObj.value}</strong></br> <i>Letzte Chance zur Abholung in der Sparkassenpassage in Kaufbeuren am 22.02. zwischen 10 und 13 Uhr</i></p>`
+        );
+        $(".checkResult").fadeIn("fast").css("display", "flex");
+      } else {
+        $(".checkResult > div").html("<p>Leider kein Gewinn gefunden</p>");
+        $(".checkResult").fadeIn("fast").css("display", "flex");
+      }
+    });
+    $(".checkNumber").keypress(function (e) {
+      if (e.which == 13) {
+        $(".checkNumberSubmit").click();
+      }
+    });
+    $(".checkResult > span").click(function () {
+      $(".checkResult").fadeOut("fast");
+    });
+  }
+}
 
 var position = $(window).scrollTop();
 
@@ -37,17 +78,11 @@ $(".index").scroll(function (event) {
   var st = $(this).scrollTop();
   if (st > wHeight * 0.25) {
     // downscroll code
-    $(".overlay").attr(
-      "style",
-      "opacity: " + 1 + "!important;"
-    );
+    $(".overlay").attr("style", "opacity: " + 1 + "!important;");
     $("body").addClass("scrolled");
   } else {
     // topscroll code
-    $(".overlay").attr(
-      "style",
-      "opacity: " + 0 + "!important;"
-    );
+    $(".overlay").attr("style", "opacity: " + 0 + "!important;");
     $("body").removeClass("scrolled");
   }
 });
@@ -71,10 +106,9 @@ function calender() {
         $(".modal.active").removeClass("active");
       });
       let cnum = $(this).text();
-      if($("#km" + cnum)[0]){
+      if ($("#km" + cnum)[0]) {
         $("#km" + cnum).addClass("active");
       }
-      
     });
   });
 }
